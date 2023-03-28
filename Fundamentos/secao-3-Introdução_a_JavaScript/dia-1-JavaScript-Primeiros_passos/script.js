ad125754db535d8e25d67e300d0bd92056ee0b79
Otaciso silva <otacisods@gmail.com>
2023-03-28 18:36:44 -0300

    Variáveis

---
+++ Fundamentos/secao-3-Introdução_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
@@ -0,0 +1,10 @@
+let birthYear = 1988;
+let myName = "Otaciso Silva";
+let birthCity = "Rio Tinto";
+const age = 34;
+
+
+console.log(birthYear);
+console.log(myName);
+console.log(birthCity);
+console.log(age);
