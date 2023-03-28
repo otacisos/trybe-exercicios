529ddafe3fccfa51b91ede5b07aaf2f404cbcc44
Otaciso silva <otacisods@gmail.com>
2023-03-28 18:58:32 -0300

    Tipos Primitivos

--- Fundamentos/secao-3-Introdução_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
+++ Fundamentos/secao-3-Introdução_a_JavaScript/dia-1-JavaScript-Primeiros_passos/script.js
@@ -3,8 +3,16 @@ let myName = "Otaciso Silva";
 let birthCity = "Rio Tinto";
 const age = 34;
 
+let patientId = 50;
+let isEnrolled = true;
+let patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+let patientEmail = 'ana@email.com';
 
 console.log(birthYear);
 console.log(myName);
 console.log(birthCity);
 console.log(age);
+console.log(typeof patientAge)
