13c21cee26727b8dbf737900d12852e804b55a8a
Otaciso silva <otacisods@gmail.com>
2023-03-29 10:43:44 -0300

    Praticando o for

---
+++ Fundamentos/secao-3-Introdução_a_JavaScript/dia-2-JavaScript-Array_e_loop_For/for.js
@@ -0,0 +1,15 @@
+let cars = ["Saab", "Volvo", "BMW"];
+
+for (let index = 0; index < cars.length; index ++) {
+  console.log(cars[index]);
+}
+
+//Utilize o for para imprimir os elementos
+//da lista groceryList com o console.log():
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.length; index++) {
+  console.log(groceryList[index]);
+
+}
