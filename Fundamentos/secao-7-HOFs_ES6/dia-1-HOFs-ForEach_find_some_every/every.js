f37e697216e2e5bebd497a21a62a6a81a4df4792
otaciso silva <otacisods@gmail.com>
2023-05-05 15:42:55 -0300

    Brincando com array.every

---
+++ Fundamentos/secao-7-HOFs_ES6/dia-1-HOFs-ForEach_find_some_every/every.js
@@ -0,0 +1,29 @@
+const grades = {
+  portugues: 'Aprovado',
+  matematica: 'Reprovado',
+  ingles: 'Aprovado',
+};
+
+const verifyGrades = Object.values(grades).every((grade) => grade === 'Aprovado'); // false
+
+// console.log(verifyGrades);
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+// Crie uma função que verifica se todas as pessoas do array 
+// people possuem a idade mínima especificada;
+
+// Retorne true se todas tiverem a idade maior ou 
+// igual à mínima e, caso contrário, false.
+
+//Adicione seu código aqui
+const verifyAges = () => people.some((ages) => ages.age >= 18);
+
+console.log(verifyAges(people, 18));
+console.log(verifyAges(people, 14));
