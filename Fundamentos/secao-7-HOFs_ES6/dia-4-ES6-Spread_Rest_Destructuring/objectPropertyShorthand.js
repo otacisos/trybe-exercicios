3094c5d159f3a9848248606f520575aa591fc27c
otaciso silva <otacisods@gmail.com>
2023-05-10 19:45:48 -0300

    Object Property Shorthand

---
+++ Fundamentos/secao-7-HOFs_ES6/dia-4-ES6-Spread_Rest_Destructuring/objectPropertyShorthand.js
@@ -0,0 +1,16 @@
+const newUser = (id, name, email) => {
+  return {
+    id: id,
+    name: name,
+    email: email,
+  };
+};
+
+// console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
+
+const getPosition = (latitude, longitude) => ({
+  latitude,
+  longitude,
+});
+
+console.log(getPosition(-19.8157, -43.9542));
