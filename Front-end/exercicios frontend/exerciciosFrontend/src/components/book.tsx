8d01eaf2dbfeb10314d49867ea877e90b4411264
otaciso silva <otacisods@gmail.com>
2023-05-31 19:50:23 -0300

    Estudando Props

---
+++ Front-end/exercicios frontend/exerciciosFrontend/src/components/book.tsx
@@ -0,0 +1,5 @@
+function Book() {
+  return <li>Book</li>;
+}
+
+export default Book;
--- Front-end/exercicios frontend/exerciciosFrontend/src/components/helloWorld.tsx
+++ Front-end/exercicios frontend/exerciciosFrontend/src/components/helloWorld.tsx
@@ -1,15 +1,18 @@
 import Title from './title';
 import ModuleDetails from './moduleDetails';
 import { getAge } from '../utils/age';
+import User from './user';
 
-const name = 'Otaciso Silva';
+// const name = 'Otaciso Silva';
 const ani = '01/08/1988';
 const ages = getAge(ani);
 
 function HelloWorld() {
   return (
     <div>
-      <h1>{`${name}`}</h1>
+      Óla,
+      {' '}
+      <User />
       <Title />
       <ModuleDetails />
       <p>{`Age ${ages}`}</p>
