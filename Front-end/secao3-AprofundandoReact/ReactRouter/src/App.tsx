6e903713eb45a45f50a031cc75752fa82780cb66
otaciso silva <otacisods@gmail.com>
2023-06-13 14:22:56 -0300

    2 Parte dos exercicios sobre router

--- Front-end/secao3-AprofundandoReact/ReactRouter/src/App.tsx
+++ Front-end/secao3-AprofundandoReact/ReactRouter/src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
 import CoffeeList from './pages/coffee-list';
+import NavBar from './nav-bar';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Home /> } />
-      <Route path="/cofees" element={ <CoffeeList /> } />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+        <Route path="/cofees" element={ <CoffeeList /> } />
+      </Routes>
+    </>
   );
 }
 
